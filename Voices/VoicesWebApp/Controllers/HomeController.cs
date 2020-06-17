622188855e61177190edc8ee23989808a62d75fc
KB <K.B_Bryant@icloud.com>
2020-06-16 23:03:44 -0700

    Need to fix the login page, front end clean up...

--- Voices/VoicesWebApp/Controllers/HomeController.cs
+++ Voices/VoicesWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using Voices.Models;
@@ -23,7 +24,15 @@ namespace Voices.Controllers
         {
             return View();
         }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
         public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
--- Voices/VoicesWebApp/ViewModels/UsersViewModel.cs
+++ Voices/VoicesWebApp/ViewModels/UsersViewModel.cs
@@ -8,19 +8,31 @@ namespace VoicesWebUI.ViewModels
         public int UserId { get; set; }
 
         [Required]
+        [MinLength(2, ErrorMessage = "First Name must be at least 2 chars")]
+        [MaxLength(30, ErrorMessage = "Too many chars")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [MinLength(2, ErrorMessage = "Last Name must be at least 2 chars")]
+        [MaxLength(30, ErrorMessage = "Too many chars")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [MinLength(2, ErrorMessage = "Username can't be empty")]
+        [MaxLength(20, ErrorMessage = "Username too long")]
         public string Username { get; set; }
         [Required]
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "E-Mail")]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be longer than 5 chars")]
+        [MaxLength(20, ErrorMessage = "Password too long, Must be shorter than 20 chars")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
 
     }
 }
